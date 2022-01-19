6d7269a8487dc2e45314eaf6733569f642abf36a
Sarah Usher <sarahnusher@gmail.com>
2022-01-19 18:58:55 +0000

    add 30 test + refactor. Level 1 completed

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # purple_sorting_balls
+
+From:
+http://codekata.com/kata/kata11-sorting-it-out/
+
+
+Level 1 - can use in-built functions
+Level 2 - cannot use inbuilt functions
--- test_sorting_balls.py
+++ test_sorting_balls.py
@@ -1,11 +1,10 @@
 from rack import Rack
 
 
-
 def test_rack():
     rack = Rack()
     rack.add(20)
     rack.add(10)
+    rack.add(30)
 
-    assert rack.balls() == [10,20] 
-
+    assert rack.balls() == [10, 20, 30]
