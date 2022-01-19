cfc23e009fcf113488ab7889a0900411c9b3478a
Sarah Usher <sarahnusher@gmail.com>
2022-01-19 18:11:10 +0000

    first test started

---
+++ rack.py
@@ -0,0 +1,5 @@
+class Rack:
+    rack = []
+
+    def add(self, ball):
+        pass
--- test_sorting_balls.py
+++ test_sorting_balls.py
@@ -1,5 +1,12 @@
+from rack import Rack
 from sorting_balls import inc
 
 
 def test_answer():
     assert inc(3) == 5
+
+def test_rack():
+    rack = Rack()
+    rack.add(10)
+
+    assert rack.balls() == [10]
